9e3964418acebd6111a1a039ddbbec041495ea1a
aakritichapain <akriteechapain@gmail.com>
2019-12-19 14:32:52 +0545

    UserModel added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-  },
-  //extends: [
-    //'airbnb-base',
-  //],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    "semi":["warn","always"],
-    "quotes":["warn","double"],
-    "no-console":"warn",
-    "camelcase":"warn"
-  },
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
 };
---
+++ Models/UserModel.js
@@ -0,0 +1,57 @@
+
+
+var dbConfig= require('../Config/databaseConfig.js')
+console.log(dbConfig.Sequelize)
+
+
+var user= dbConfig.sequelize.define('user',
+//attributes
+{
+	id:{
+		type:dbConfig.Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+		allowNull: false
+	},
+	username:{
+		type:dbConfig.Sequelize.TEXT,
+		allowNull: false
+	},
+	phone:{
+		type:dbConfig.Sequelize.TEXT,
+		allowNull: false
+	},
+	address:{
+		type:dbConfig.Sequelize.TEXT,
+		allowNull: false
+	},
+	email:{
+		type:dbConfig.Sequelize.TEXT,
+		allowNull: false
+	},
+	password:{
+		type:dbConfig.Sequelize.TEXT,
+		allowNull: false
+}
+},
+/*{
+	freezeTable:true, //tablename chnage garna yeo freezeTable garekooo
+	tableName:'user_table' //tablename changed
+
+
+}*/
+
+
+	)
+
+user.sync({force:false})
+.then(function(result){
+	console.log(result);
+
+
+})
+.catch(function(err){
+	console.log(err);
+})
+
+module.exports=user;
